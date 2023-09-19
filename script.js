a7d814e02c4c1b63cd0e551ea7be387181addba3
marialuizarodrigue2 <137801199+marialuizarodrigue2@users.noreply.github.com>
2023-09-19 11:43:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+calcule.addEventListener('click',function(){
+
+   var a = valorA.value;
+   var b = valorB.value;
+
+   a= parseInt(a);
+   b= parseInt(b);
+
+   var result = a + b;
+   var result = a - b;
+   var result = a / b;
+
+
+    alert(result);
+
+
+
+
+
+});
